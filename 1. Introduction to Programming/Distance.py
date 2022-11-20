c38a699ba98cde08f9a75b2c323628e1d63dc36f
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-11-20 14:02:47 +0600

    Added new exercise.

---
+++ 1. Introduction to Programming/Distance.py
@@ -0,0 +1,7 @@
+dist = int(input("Enter a distance in feets: "))
+
+feet_in_inch = dist * 12
+feet_in_yard = dist / 3
+feet_in_mile = dist / 5280
+
+print(f"Inches: {feet_in_inch}\nYards: {feet_in_yard}\nMiles: {feet_in_mile}")
